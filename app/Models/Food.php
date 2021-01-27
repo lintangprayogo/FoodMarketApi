6102c1497f788fa64f6b2108e3122ff8f7c31e9a
Lintang Prayogo <lintangprayogo12@gmail.com>
2021-01-27 13:27:55 +0700

    database migration and model

---
+++ app/Models/Food.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\Storage;
+
+class Food extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name', 'description', 'ingredients', 'price', 'rate', 'types', 'picturePath'
+    ];
+
+    public function getCreatedAtAttribute($value)
+    {
+        return Carbon::parse($value)->timestamp;
+    }
+    public function getUpdatedAtAttribute($value)
+    {
+        return Carbon::parse($value)->timestamp;
+    }
+
+    public function toArray()
+    {
+        $toArray = parent::toArray();
+        $toArray['picturePath'] = $this->toArray();
+        return $toArray;
+    }
+
+    public function getpicturePathAttribute()
+    {
+        return url('') . Storage::url($this->attributes['picturePath']);
+    }
+}
---
+++ database/migrations/2021_01_26_164035_create_transactions_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('food_id');
+            $table->integer('quantity');
+            $table->integer('total');
+            $table->string('status');
+            $table->string('payment_url');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('food_id')->references('id')->on('foods');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
